22a157322fda2861d56c97bae737a3591816b75f
bessonovaka <bessonova.en@mail.ru>
2021-10-20 17:04:21 +0300

    feat: настроить динамические маршруты

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // импортируем BrowserRouter
-import App from './components/App';
 import './index.css';
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
-// теперь обернём компонент App в BrowserRouter
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter> 
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+
+serviceWorker.unregister();
