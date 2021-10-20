01d42cdc670d61eb77dd1c1b1dc1ae748b421989
bessonovaka <bessonova.en@mail.ru>
2021-10-20 14:06:27 +0300

    feat: настроить роутер

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-
 import './App.css';
+import Dashboard from './Dashboard';
 import Header from './Header';
+import Reviews from './Reviews';
+import AboutMe from './AboutMe';
+
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Route path="/">
+        <Dashboard />
+      </Route>
+      <Route path="/reviews">
+        <Reviews />
+      </Route>
+      <Route path="/about-me">
+        <AboutMe />
+      </Route>
     </div>
   );
 }
 
 export default App;
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
-function Header() {
-    return(
-        <header></header>
-    )
+import React from 'react';
+import './Header.css';
+import Logo from './Logo';
+
+
+function Header () {
+  return (
+    <div className="header">
+      <Logo />
+    </div>
+  )
 }
 
 export default Header;
